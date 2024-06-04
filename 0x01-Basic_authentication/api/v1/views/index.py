10b18d93d2d219254679a6d863d4377235ea34a6
seifbenothman <seifbenothman95@gmail.com>
2024-06-04 23:45:48 +0100

    crez

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,19 +24,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> None:
-    """ GET /api/v1/unauthorized
-    Raise a 401 error
-    """
-    abort(401)
-
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> None:
-    """ GET /api/v1/forbidden
-    Raise a 403 error
-    """
-    abort(403)
