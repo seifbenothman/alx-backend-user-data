8f55d21b97b368d5ad42907ceaa1d95e21b0012e
seifbenothman <seifbenothman95@gmail.com>
2024-06-04 23:21:44 +0000

    up

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth module for the API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -11,7 +12,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
             self, authorization_header: str
             ) -> str:
-        """Extracts the Base64 part of the Authorization"""
+        """ Extracts Base64 """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """ Decodes a Base64 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
+            return None
